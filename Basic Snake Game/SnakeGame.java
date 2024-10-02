e815fec898596a1def6c8c7762f5167ec6dd8f1c
NaveenRajk-DEV <naveen6863855@gmail.com>
2024-10-02 16:56:50 +0530

    Add files via upload

--- Basic Snake Game/SnakeGame.java
+++ Basic Snake Game/SnakeGame.java
@@ -1,1 +1,23 @@
+import javax.swing.JFrame;
+
+public class SnakeGame extends JFrame {
+
+    public SnakeGame() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new GameBoard());
+        setResizable(false);
+        pack();
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        JFrame ex = new SnakeGame();
+        ex.setVisible(true);
+    }
+}
 
